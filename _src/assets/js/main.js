7945e1b29b4d7ff30c9e3d696d559c63efb11e8c
arijdb <ariane.jurado@gmail.com>
2019-04-22 10:51:11 +0200

    add default image ok

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,10 +5,8 @@ console.log('>> Ready :)');
 const field = document.querySelector('.field');
 const btn = document.querySelector('.btn');
 const list = document.querySelector('.list');
-// const value = field.value;
 const apiBase = 'http://api.tvmaze.com/search/shows?q=';
 
-//función buscar
 const search = () => {
   const api = apiBase + field.value;
   fetch(api)
@@ -16,27 +14,31 @@ const search = () => {
     .then(data => {
       for (let i = 0; i < data.length; i++) {
         const item = data[i];
-        console.log('ITEM', data[i]);
         const showItem = item.show;
-        console.log('data', showItem);
         const showTitle = showItem.name;
-        console.log('NAME', showTitle);
         const showImages = showItem.image;
-        console.log('IMAGE', showImages);
-        const showImage = showImages.medium;
-        console.log('MEDIUM', showImage);
 
         const image = document.createElement('img');
         const name = document.createElement('h2');
         const itemShow = document.createElement('li');
         itemShow.setAttribute('class', 'item');
-        image.setAttribute('src', `${showImage}`);
+
         name.setAttribute('class', 'name');
         const nameText = document.createTextNode(showTitle);
         name.appendChild(nameText);
         itemShow.appendChild(image);
         itemShow.appendChild(name);
         list.appendChild(itemShow);
+        //si no tiene foto
+        if (showImages === null) {
+          image.setAttribute(
+            'src',
+            'https://via.placeholder.com/210x295/ffffff/666666/?text=TV'
+          );
+        } else if (showImages !== null) {
+          const showImage = showImages.medium;
+          image.setAttribute('src', `${showImage}`);
+        }
       }
     });
 };

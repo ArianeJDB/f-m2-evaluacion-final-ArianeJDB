1f40f708e968b0eaf3de243295f6ce2d9fa9b5f1
arijdb <ariane.jurado@gmail.com>
2019-04-22 16:11:36 +0200

    list favourites done

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,14 +1,16 @@
 'use strict';
 
-console.log('>> Ready :)');
-
 const field = document.querySelector('.field');
 const btn = document.querySelector('.btn');
 const list = document.querySelector('.list');
 const apiBase = 'http://api.tvmaze.com/search/shows?q=';
+const fav = [];
+const favList = document.querySelector('.fav__list');
 
 const search = () => {
+  list.innerHTML = ' ';
   const api = apiBase + field.value;
+
   fetch(api)
     .then(response => response.json())
     .then(data => {
@@ -17,15 +19,19 @@ const search = () => {
         const showItem = item.show;
         const showTitle = showItem.name;
         const showImages = showItem.image;
-
+        const showImage = showImages.medium;
         const image = document.createElement('img');
         const name = document.createElement('h2');
         const itemShow = document.createElement('li');
         itemShow.setAttribute('class', 'item');
 
         name.setAttribute('class', 'name');
+        image.setAttribute('class', 'img');
         const nameText = document.createTextNode(showTitle);
         name.appendChild(nameText);
+
+        itemShow.addEventListener('click', function(e) {favouriteShow(e, showTitle, showImage);}); //estos parametros se los pasa como "name" y como "image" a la funcion favouriteshow
+
         itemShow.appendChild(image);
         itemShow.appendChild(name);
         list.appendChild(itemShow);
@@ -40,24 +46,39 @@ const search = () => {
           image.setAttribute('src', `${showImage}`);
         }
       }
-      const li = document.querySelectorAll('.item');
-      for (const liItem of li) {
-        const favouriteShow = e => {
-          const trigger = e.currentTarget;
-          if (trigger.classList.contains('item')) {
-            trigger.classList.remove('item');
-            trigger.classList.add('item__favourite');
-          } else {
-            trigger.classList.add('item');
-            trigger.classList.remove('item__favourite');
-          }
-        };
-  
-        liItem.addEventListener('click', favouriteShow);
-      }
-
-      
+     
     });
 };
+
+function favouriteShow(e, name, image) {//aqui tenfo acceso al titulo de la pelicula de la peticion, la imagen y el evento del click
+
+  const trigger = e.currentTarget;
+  if (trigger.classList.contains('item')) {
+    trigger.classList.remove('item');
+    trigger.classList.add('item__favourite'); //si la selecciono como fav, la meto en el div de fav azul. o rosa.
+  } else {
+    trigger.classList.add('item');
+    trigger.classList.remove('item__favourite');
+  }
+  //añadir al array vacío fav[]
+
+  if (trigger.classList.contains('item__favourite')) {
+    fav.push(trigger);
+  }
+  // } else {
+  //   fav.pop(trigger);
+  // }
+  console.log(fav);
+
+  const imageFav = document.createElement('img');
+  const nameFav = document.createElement('h2');
+  const liFav = document.createElement('li');
+  imageFav.setAttribute('src', image);
+  const nameText = document.createTextNode(name);
+  nameFav.appendChild(nameText);
+  liFav.appendChild(nameFav);
+  liFav.appendChild(imageFav);
+  favList.appendChild(liFav);
+}
 
 btn.addEventListener('click', search);

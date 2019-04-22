3a8da3107434789717cd346fe773e3d89c96933a
arijdb <ariane.jurado@gmail.com>
2019-04-22 09:37:23 +0200

    done search and paint

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -8,15 +8,12 @@ const list = document.querySelector('.list');
 // const value = field.value;
 const apiBase = 'http://api.tvmaze.com/search/shows?q=';
 
-
 //función buscar
 const search = () => {
   const api = apiBase + field.value;
   fetch(api)
     .then(response => response.json())
     .then(data => {
-      
-     
       for (let i = 0; i < data.length; i++) {
         const item = data[i];
         console.log('ITEM', data[i]);
@@ -28,7 +25,18 @@ const search = () => {
         console.log('IMAGE', showImages);
         const showImage = showImages.medium;
         console.log('MEDIUM', showImage);
-        
+
+        const image = document.createElement('img');
+        const name = document.createElement('h2');
+        const itemShow = document.createElement('li');
+        itemShow.setAttribute('class', 'item');
+        image.setAttribute('src', `${showImage}`);
+        name.setAttribute('class', 'name');
+        const nameText = document.createTextNode(showTitle);
+        name.appendChild(nameText);
+        itemShow.appendChild(image);
+        itemShow.appendChild(name);
+        list.appendChild(itemShow);
       }
     });
 };

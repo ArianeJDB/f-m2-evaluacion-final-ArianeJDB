41ce30ef162aa3b5a54c5da6fd27aba402a2ac81
arijdb <ariane.jurado@gmail.com>
2019-04-22 09:19:20 +0200

    fetch ok

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+const field = document.querySelector('.field');
+const btn = document.querySelector('.btn');
+const list = document.querySelector('.list');
+// const value = field.value;
+const apiBase = 'http://api.tvmaze.com/search/shows?q=';
+
+
+//función buscar
+const search = () => {
+  const api = apiBase + field.value;
+  fetch(api)
+    .then(response => response.json())
+    .then(data => {
+      
+     
+      for (let i = 0; i < data.length; i++) {
+        const item = data[i];
+        console.log('ITEM', data[i]);
+        const showItem = item.show;
+        console.log('data', showItem);
+        const showTitle = showItem.name;
+        console.log('NAME', showTitle);
+        const showImages = showItem.image;
+        console.log('IMAGE', showImages);
+        const showImage = showImages.medium;
+        console.log('MEDIUM', showImage);
+        
+      }
+    });
+};
+
+btn.addEventListener('click', search);
